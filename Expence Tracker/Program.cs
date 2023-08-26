548e7b663b37b26955870ce192c6fd2487294d5f
shivam <supshiv7250@gmail.com>
2023-08-26 16:12:13 +0530

    Project Completed

--- Expence Tracker/Program.cs
+++ Expence Tracker/Program.cs
@@ -34,6 +34,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
 
 app.Run();
